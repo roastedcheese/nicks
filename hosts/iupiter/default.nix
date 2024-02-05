37fbfbe71bc460af6ba11c95df64a7cff46d8ddf
roastedcheese <roastedcheesee@protonmail.com>
2024-02-05 12:15:27 +0100

    hosts/iupiter: downgrade to linux_5_4 because of nvidia moment
    https://nixpk.gs/pr-tracker.html\?pr\=286084

--- hosts/iupiter/default.nix
+++ hosts/iupiter/default.nix
@@ -1,10 +1,16 @@
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxKernel.packages.linux_5_4; # Nvidia moment https://forums.developer.nvidia.com/t/linux-6-7-3-545-29-06-550-40-07-error-modpost-gpl-incompatible-module-nvidia-ko-uses-gpl-only-symbol-rcu-read-lock/280908
+  };
 
   networking.hostName = "iupiter";
 }
--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -35,6 +35,6 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
