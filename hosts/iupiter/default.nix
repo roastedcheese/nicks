ea43bb5b3fdd0de751b2158b51af80d51cb0b366
roastedcheese <roastedcheesee@protonmail.com>
2024-02-07 13:26:57 +0100

    flake/inputs: bump
    also the nvidia moment patch got into the cache so upgrade back to the latest kernel
    
    note that from now on I will be bumping npins together with the inputs without mentioning it in the commit message

--- hosts/iupiter/default.nix
+++ hosts/iupiter/default.nix
@@ -1,4 +1,3 @@
-{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -9,7 +8,6 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxKernel.packages.linux_5_4; # Nvidia moment https://forums.developer.nvidia.com/t/linux-6-7-3-545-29-06-550-40-07-error-modpost-gpl-incompatible-module-nvidia-ko-uses-gpl-only-symbol-rcu-read-lock/280908
   };
 
   networking.hostName = "iupiter";
