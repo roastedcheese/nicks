28aed9ad85094e2b307963086f360e83719a9278
roastedcheese <roastedcheesee@protonmail.com>
2024-02-11 19:15:57 +0100

    home/ags: automatically fetch coords for weather module

--- home/programs/desktop/wayland/ags/config/js/weather.js
+++ home/programs/desktop/wayland/ags/config/js/weather.js
@@ -1,10 +1,16 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 import { exec, execAsync } from 'resource:///com/github/Aylur/ags/utils.js';
-import { POS } from './pos.js';
 const { Box, Button, Stack, Label, Icon, CenterBox, Window, Slider, ProgressBar } = Widget;
 
-const FETCH = `https://api.open-meteo.com/v1/forecast?latitude=${POS[0]}&longitude=${POS[1]}&current=temperature_2m,is_day,rain,showers,snowfall,weathercode,cloudcover&timezone=Europe%2FBerlin&forecast_days=1`
+let fetch;
+execAsync('curl http://ip-api.com/json/').then(out => {
+  let o = JSON.parse(out);
+  const POS = [o.lat, o.lon];
+  fetch = `https://api.open-meteo.com/v1/forecast?latitude=${POS[0]}&longitude=${POS[1]}&current=temperature_2m,is_day,rain,showers,snowfall,weathercode,cloudcover&timezone=Europe%2FBerlin&forecast_days=1`;
+}).catch(console.error);
+// "ummmm but this allows for a race condition" I don't care, this  module is useless anyways
+
 
 export const Weather = () => Button({
   className: 'weather',
@@ -12,6 +18,7 @@ export const Weather = () => Button({
     children: [
       Stack({
         items: [
+          ['', Label('')],
           ['clear', Icon('weather-clear-symbolic')],
           ['clear-night', Icon('weather-clear-night-symbolic')],
           ['cloudy', Icon('weather-few-clouds-symbolic')],
@@ -26,7 +33,7 @@ export const Weather = () => Button({
       Label('')
     ],
     connections: [[60000, self => {
-      execAsync(`curl ${FETCH}`).then(out => {
+      execAsync(`curl ${fetch}`).then(out => {
         let fc = JSON.parse(out);
         let day = fc.current.is_day;
         let rain = fc.current.rain;
@@ -40,17 +47,17 @@ export const Weather = () => Button({
         if (clouds > 25)
             output = 'cloudy';
         else
-            output = 'clear';
+          output = 'clear';
         if (day === 0)
-            output += '-night';
+          output += '-night';
         if (clouds > 88)
-            output = 'overcast';
+          output = 'overcast';
         if (rain !== 0)
-            output = 'rainy';
+          output = 'rainy';
         if (snow !== 0)
-            output = 'snowy';
+          output = 'snowy';
         if (wc > 94)
-            output = 'thunder';
+          output = 'thunder';
 
         self.children[0].shown = output;
       }).catch(console.error);
