273e15745bd5e48afafb2ac12e60580ec972fed8
RoastedCheese <cheese@roastedcheese.org>
2024-02-23 17:30:52 +0100

    home/hyprland: add wallpapers

--- home/desktop/wayland/hyprland/scripts/wpnext.sh
+++ home/desktop/wayland/hyprland/scripts/wpnext.sh
@@ -8,5 +8,5 @@ if [ $i = $((max + 1)) ]; then
     i=1
 fi
 
-swww img ~/Media/Pictures/walls/active/$i* -t random
+swww img ~/.config/hypr/scripts/wallpapers/$i* -t random
 echo $i > ~/.config/hypr/scripts/wallpaper
