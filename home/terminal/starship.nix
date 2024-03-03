131ffb993a2bc08d555b18224e0b80e00d51d5f9
RoastedCheese <cheese@roastedcheese.org>
2024-03-03 20:04:15 +0100

    home/terminal: fix newline after clear
    why would anyone want this?
    
    https://github.com/starship/starship/issues/560

--- home/terminal/starship.nix
+++ home/terminal/starship.nix
@@ -1,10 +1,8 @@
 {
   programs.starship = {
     enable = true;
-    enableZshIntegration = true;
-
     settings = {
-      add_newline = true;
+      add_newline = false;
       line_break.disabled = true;
       cmd_duration.format = "[$duration]($style) ";
       git_branch.format = "[$symbol$branch(:$remote_branch)]($style) ";
