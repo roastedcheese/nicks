6d9a803b0ea99b7c38e4599c90377b5a70809844
RoastedCheese <cheese@roastedcheese.org>
2024-04-02 01:25:17 +0200

    system/networking: ag

--- system/core/networking.nix
+++ system/core/networking.nix
@@ -10,22 +10,23 @@
         address_data = "194.242.2.2";
         tls_auth_name = "dns.mullvad.net";
       }
-      {
-        address_data = "45.90.28.0";
-        tls_auth_name = "9f36d4.dns.nextdns.io";
-      }
-      {
-        address_data = "2a07:a8c0::0";
-        tls_auth_name = "9f36d4.dns.nextdns.io";
-      }
-      {
-        address_data = "45.90.30.0";
-        tls_auth_name = "9f36d4.dns.nextdns.io";
-      }
-      {
-        address_data = "2a07:a8c1::0";
-        tls_auth_name = "9f36d4.dns.nextdns.io";
-      }];
+      # {
+      #   address_data = "45.90.28.0";
+      #   tls_auth_name = "9f36d4.dns.nextdns.io";
+      # }
+      # {
+      #   address_data = "2a07:a8c0::0";
+      #   tls_auth_name = "9f36d4.dns.nextdns.io";
+      # }
+      # {
+      #   address_data = "45.90.30.0";
+      #   tls_auth_name = "9f36d4.dns.nextdns.io";
+      # }
+      # {
+      #   address_data = "2a07:a8c1::0";
+      #   tls_auth_name = "9f36d4.dns.nextdns.io";
+      # }
+      ];
     };
   };
 }
