27bd430312c9a1f043674fe893d2dfce060db477
RoastedCheese <cheese@roastedcheese.org>
2024-05-03 21:46:47 +0200

    system/nix: steal from raf
    remove useless module

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,54 +1,56 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, inputs, pkgs, ... }:
 let
-  inherit (lib) mkOption mkIf types mkEnableOption;
-  cfg = config.opt;
-  semanticConfType = with types;
-    let
-      confAtom = nullOr
-        (oneOf [
-          bool
-          int
-          float
-          str
-          path
-          package
-        ]) // {
-        description = "Nix config atom (null, bool, int, float, str, path or package)";
-      };
-    in
-    attrsOf (either confAtom (listOf confAtom));
+  inherit (lib) mapAttrs mkIf;
 in 
 {
-  options.opt = {
-    nix = {
-      nixPath = mkOption {
-        type = types.listOf types.str;
-        default = [ "nixpkgs=flake:nixpkgs" ];
-      };
-      settings = mkOption {
-        type = types.submodule {
-          freeformType = semanticConfType;
-
-          options = {
-            auto-optimise-store = mkOption {
-              type = types.bool;
-              default = true;
-            };
-            experimental-features = mkOption {
-              type = types.listOf types.str;
-              default = [ "nix-command" "flakes" ];
-            };
-          };
-        };
-        default = {};
-      };
-    };
-
-    nixpkgs.config.allowUnfree = mkEnableOption "non-free packages";
-  };
 
   config = { 
-    inherit (cfg) nixpkgs nix; 
-    environment.systemPackages = mkIf config.nix.enable [ pkgs.git ];
+    environment.systemPackages = [ pkgs.git ];
+
+    nix = let 
+      registry = mapAttrs (_: v: { flake = v; }) inputs;
+    in {
+      nixPath = [ "nixpkgs=flake:nixpkgs" ];
+      inherit registry;
+
+      gc = {
+        automatic = true;
+        dates = "Mon *-*-* 03:00";
+        options = "--delete-older-than 7d";
+      };
+
+      optimise = {
+        automatic = true;
+        dates = [ "04:00" ];
+      };
+
+      settings = {
+        allowed-users = ["root" "@wheel" "nix-builder"];
+        trusted-users = ["root" "@wheel" "nix-builder"];
+
+        auto-optimise-store = true;
+        experimental-features = [ "nix-command" "flakes" ];
+        use-xdg-base-directories = true;
+
+        warn-dirty = false;
+        sandbox = true;
+        sandbox-fallback = false;
+        builders-use-substitutes = true;
+
+        substituters = [
+          "https://cache.nixos.org"
+          "https://nix-community.cachix.org"
+          (mkIf config.opt.programs.hyprland.enable "https://hyprland.cachix.org")
+          (mkIf config.opt.programs.ags.enable "https://ags.cachix.org")
+        ];
+
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          (mkIf config.opt.programs.hyprland.enable "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=")
+          (mkIf config.opt.programs.ags.enable "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8=")
+        ];
+      };
+    };
   };
 }
