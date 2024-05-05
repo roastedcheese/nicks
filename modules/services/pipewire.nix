b83c3197bb486079ce9fb9c6f34a66859d0345ca
RoastedCheese <cheese@roastedcheese.org>
2024-05-05 13:01:48 +0000

    services/pipewire: add noisetorch

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -2,19 +2,26 @@
 let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.opt.services.pipewire;
+  home = config.home-manager.users.${config.opt.system.username};
 in 
 {
-  options.opt.services.pipewire.enable = mkEnableOption "PipeWire sound server";
+  options.opt.services.pipewire = {
+    enable = mkEnableOption "PipeWire sound server";
+    noiseTorch = mkEnableOption "NoiseTorch for noise reduction";
+  };
 
   config = mkIf cfg.enable {
-    opt.home.packages = [ pkgs.pulseaudio ];
-    services.pipewire = {
-      enable = true;
-      pulse.enable = true;
-      jack.enable = true;
-      alsa = {
+    opt.home.packages = with pkgs; [ pulseaudio ];
+    programs.noisetorch.enable = mkIf cfg.noiseTorch true;
+    services = {
+      pipewire = {
         enable = true;
-        support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+        alsa = {
+          enable = true;
+          support32Bit = true;
+        };
       };
     };
   };
