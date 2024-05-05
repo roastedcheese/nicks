ab2e4e9d59d95076048b9070ac01d0930a5ba6b4
RoastedCheese <cheese@roastedcheese.org>
2024-05-05 10:49:34 +0000

    programs/nh: oops
    I forgor

--- modules/programs/nh.nix
+++ modules/programs/nh.nix
@@ -2,11 +2,12 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.opt.programs.nh;
+  home = config.home-manager.users.${config.opt.system.username}.home.homeDirectory;
 in 
 {
   options.opt.programs.nh.enable = mkEnableOption "nh nix helper";
   config.programs.nh = mkIf cfg.enable {
     enable = true;
-    flake = "${inputs.self}";
+    flake = "${home}/nicks";
   };
 }
