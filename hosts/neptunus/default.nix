06fa5d225a304183daa2f481f7996dcff0fc0dde
RoastedCheese <cheese@roastedcheese.org>
2024-06-01 23:42:11 +0200

    programs/beets: add fetchart plugin

--- hosts/neptunus/default.nix
+++ hosts/neptunus/default.nix
@@ -35,6 +35,10 @@
       enable = true;
       domain = "nromano.net";
     };
+    programs.beets = {
+      enable = true;
+      musicDir = "/srv/music";
+    };
     system.roles.headless = true;
   };
 
