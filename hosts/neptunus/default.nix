0bd6ec6cc12ee4ccfed43d3c368b22bfb176a810
RoastedCheese <cheese@roastedcheese.org>
2024-06-01 19:01:41 +0200

    services/navidrome: init
    also self-dox yay

--- hosts/neptunus/default.nix
+++ hosts/neptunus/default.nix
@@ -9,12 +9,34 @@
   nixpkgs.hostPlatform = "aarch64-linux";
   networking.hostName = "neptunus";
 
-  services.rock5b-fan-control = {
-    enable = true;
-    package = inputs.rock.packages.${pkgs.system}.fan-control;
+  services = {
+    rock5b-fan-control = {
+      enable = true;
+      package = inputs.rock.packages.${pkgs.system}.fan-control;
+    };
+
+    ddclient = {
+      package = pkgs.ddclient.overrideAttrs {
+        src = pkgs.fetchFromGitHub {
+          owner = "ddclient";
+          repo = "ddclient";
+          rev = "282bb01e1761707297eb9d47d97848b50c36ad8d";
+          sha256 = "sha256-QS1YjJq6cAIBzPv5BFtWpZE1LeaUPpxiftGt5c+qTgA=";
+        };
+      };
+      enable = true;
+      interval = "5min";
+      configFile = "/root/ddclient.conf";
+    };
   };
 
-  opt.system.roles.headless = true;
+  opt = {
+    services.navidrome = {
+      enable = true;
+      domain = "example.org";
+    };
+    system.roles.headless = true;
+  };
 
   boot = {
     loader = {
