8e78e33a3d15dbb196eadd7be569e3efb2b05691
RoastedCheese <cheese@roastedcheese.org>
2024-06-14 14:43:51 +0200

    shells/python: add packaging
    no, overriding isn't a thing

--- flake.nix
+++ flake.nix
@@ -12,8 +12,36 @@
 
       perSystem = { pkgs, config, ... }: {
         devShells.python = pkgs.mkShell { # Stuff I need to run some python scripts
-          packages = with pkgs; [
-            flac fish python3 python312Packages.mutagen
+          packages = let
+            packaging = let # Ahah imagine if you could override a python package wouldn't that be cool
+              inherit (pkgs.python311Packages) buildPythonPackage fetchPypi setuptools pyparsing six pytestCheckHook pretend wheel;
+            in buildPythonPackage rec {
+              pname = "packaging";
+              version = "20.9";
+              format = "pyproject";
+
+              src = fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-WzJ6wTINyGPcpy9FFOzAhvMRhnRLhKIwN0zB/Xdv6uU=";
+              };
+
+              nativeBuildInputs = [
+                setuptools
+              ];
+
+              propagatedBuildInputs = [ pyparsing six wheel ];
+
+              checkInputs = [
+                pytestCheckHook
+                pretend
+              ];
+
+              # Prevent circular dependency
+              doCheck = false;
+            };
+          in 
+          with pkgs; [
+            python3 flac fish python311Packages.mutagen python311Packages.mechanicalsoup packaging
           ];
 
           # I like fish
