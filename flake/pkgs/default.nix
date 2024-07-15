e5f6ce8bf7d86ed4726871f918a881272c7ac374
RoastedCheese <cheese@roastedcheese.org>
2024-07-15 12:29:39 +0200

    flake: package salmon deps and add to shell

--- flake/pkgs/default.nix
+++ flake/pkgs/default.nix
@@ -1,7 +1,13 @@
 {
-  perSystem = { pkgs, lib, ... }: {
+  perSystem = { inputs', pkgs, lib, ... }: 
+  let
+    pkgs2 = inputs'.nixpkgs.legacyPackages.extend (_: super: lib.packagesFromDirectoryRecursive {
+      inherit (super) callPackage;
+      directory = ./packages;
+    });
+  in {
     packages = lib.packagesFromDirectoryRecursive {
-      inherit (pkgs) callPackage;
+      inherit (pkgs2) callPackage;
       directory = ./packages;
     };
   };
--- flake/shells.nix
+++ flake/shells.nix
@@ -1,37 +1,12 @@
 {
   perSystem = { self', pkgs, config, ... }: {
     devShells.python = pkgs.mkShell { # Stuff I need to run some python scripts
-      packages = let
-        packaging = let # Ahah imagine if you could override a python package wouldn't that be cool
-          inherit (pkgs.python311Packages) buildPythonPackage fetchPypi setuptools pyparsing six pytestCheckHook pretend wheel;
-        in buildPythonPackage rec {
-          pname = "packaging";
-          version = "20.9";
-          format = "pyproject";
-
-          src = fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-WzJ6wTINyGPcpy9FFOzAhvMRhnRLhKIwN0zB/Xdv6uU=";
-          };
-
-          nativeBuildInputs = [
-            setuptools
-          ];
-
-          propagatedBuildInputs = [ pyparsing six wheel ];
-
-          checkInputs = [
-            pytestCheckHook
-            pretend
-          ];
-
-          # Prevent circular dependency
-          doCheck = false;
-        };
-      in 
-      with pkgs; [
-        python3 flac fish python311Packages.mutagen python311Packages.mechanicalsoup packaging self'.packages.bcoding
-      ];
+      packages = builtins.attrValues {
+        inherit (pkgs) python3 flac fish;
+        inherit (pkgs.python3Packages) requests click beautifulsoup4 colorama musicbrainzngs mutagen pyperclip pycryptodome aiohttp
+          aiohttp-jinja2 jinja2 yaspin ratelimit rich poetry-core bitstring mechanicalsoup;
+        inherit (self'.packages) pyimgurapi heybrochecklog dottorrent bcoding packaging;
+      };
 
       # I like fish
       shellHook = ''
