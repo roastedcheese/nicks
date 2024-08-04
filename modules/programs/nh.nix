74589295ec78cc6df069be2fa6e08471ea89948b
RoastedCheese <cheese@roastedcheese.org>
2024-08-04 19:43:29 +0200

    programs/nh: enable automatic cleanup

--- modules/programs/nh.nix
+++ modules/programs/nh.nix
@@ -9,5 +9,10 @@ in
   config.programs.nh = mkIf cfg.enable {
     enable = true;
     flake = "${home}/nicks";
+    clean = {
+      enable = true;
+      dates = "Mon *-*-* 03:00";
+      extraArgs = "--keep-since 3d";
+    };
   };
 }
