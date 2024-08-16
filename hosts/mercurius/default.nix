0b3068743a06ffcb0ad33897ae5509e99c26724b
RoastedCheese <cheese@roastedcheese.org>
2024-08-16 20:30:03 +0200

    hosts/mercurius: add streamrip

--- hosts/mercurius/default.nix
+++ hosts/mercurius/default.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, ... }:
+{ pkgs, inputs, lib, ... }:
 {
   imports = [
     ./hardware-configuration.nix 
@@ -9,7 +9,25 @@
   ];
 
   time.timeZone = "Europe/Berlin";
-  opt.system.roles.headless = true;
+  opt = {
+    system.roles.headless = true;
+    home.packages = let
+      inherit (inputs.p2n.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+    in [
+      (mkPoetryApplication {
+        projectDir = pkgs.applyPatches {  
+          src = pkgs.fetchFromGitHub {
+            owner = "nathom";
+            repo = "streamrip";
+            rev = "41c0c3e3a017207f803a232b1ce214891570a1d8";
+            hash = "sha256-vYmOA5uehyLc+NkZ+Ryfri01ItvS8uOVOBtFm1nbPb0=";
+          };
+          patches = [ ./streamrip.patch ];
+        };
+      })
+      pkgs.mktorrent
+    ];
+  };
   services = {
     openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password"; # :P
 
