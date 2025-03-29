02aa6fcd9d7ab454451e0ca824fa12cf7062bdc9
RoastedCheese <cheese@roastedcheese.org>
2025-03-29 14:05:12 +0100

    hosts: update
    update secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,7 @@ let
   neptunus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILIwzO+SbSGzpaletXdJv+sGoDGP6xr9opa5CWs3tLmd";
 in {
   "mailserver.age".publicKeys = [mercurius];
+  "mailserver_2.age".publicKeys = [mercurius];
   "mailserver_matrix.age".publicKeys = [mercurius];
   "matrix_mail.age".publicKeys = [mercurius];
   "matrix_secret.age".publicKeys = [mercurius];
